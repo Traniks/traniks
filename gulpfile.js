1a24a77b7a1402054602e8ee2a1340bbae1743ee
Traniks <andert2017@gmail.com>
2023-05-25 19:08:30 +0300

    initial structure

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,30 @@ const cleanCSS      = require('gulp-clean-css');
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "./"
         }
     });
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/scss/*.+(scss|sass)")
-            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-            .pipe(rename({
-                prefix: "",
-                suffix: ".min",
-            }))
-            .pipe(autoprefixer({
-                cascade: false
-            }))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest("src/css"))
-            .pipe(browserSync.stream());
+    return gulp.src("src/scss/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+        }))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function() {
     gulp.watch("src/scss/**/*.+(scss|sass)", gulp.parallel("styles"));
     gulp.watch("src/js/**/*.js").on("change", browserSync.reload);
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("*.html").on("change", browserSync.reload);
 });
 
 gulp.task("default", gulp.parallel("watch", "server", "styles"));
