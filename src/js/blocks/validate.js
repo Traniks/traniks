260ec4b3dbeec69a7b94dfcc8eea4c739037ad78
Traniks <andert2017@gmail.com>
2023-05-28 14:47:30 +0300

    validate + post

---
+++ src/js/blocks/validate.js
@@ -0,0 +1,89 @@
+function validate() {
+    function validate_form(selector) {
+
+        const validate = new JustValidate(selector, 
+            {
+                errorLabelCssClass: ['error'],
+            }
+        );
+    
+        validate.addField("input[name=name]", [
+            {
+                rule: 'required',
+                errorMessage: "Это поле обязательно.",
+            },
+            {
+                rule: 'customRegexp',
+                value: /[A-Za-zА-Яа-яЁё]/,
+                errorMessage: "Имя может содержать только [A-z] и [А-я]."
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                errorMessage: "Имя неможет состоять из одной буквы."
+            },
+        ])
+        validate.addField("input[name=email]", [
+            {
+                rule: 'required',
+                errorMessage: "Это поле обязательно.",
+            },
+            {
+                rule: 'email',
+                errorMessage: 'Почта должна состоять из @, .com, .ru и т.д в конце',
+            },
+        ])
+        validate.addField("textarea", [
+            {
+                rule: 'required',
+                errorMessage: "Это поле обязательно.",
+            },
+        ])
+        .onSuccess((e) => {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+    
+                fadeIn(".overlay");
+                fadeIn("#thanks");
+                
+                $("form").trigger("reset");
+            });
+            console.log("Отправил форму");
+
+            
+            return false;
+        })
+    }
+
+    function fadeOut(el) {
+        let opacity = 1;
+        const timer = setInterval(function() {
+            if(opacity <= 0.1) {
+                clearInterval(timer);
+                document.querySelector(el).style.display = "none";
+            }
+            document.querySelector(el).style.opacity = opacity;
+            opacity -= opacity * 0.1;
+        }, 10);
+    }
+    function fadeIn(el) {
+        let opacity = 0.01;
+        document.querySelector(el).style.display = "block";
+        const timer = setInterval(function() {
+            if(opacity >= 1) {
+                clearInterval(timer);
+            }
+            document.querySelector(el).style.opacity = opacity;
+            opacity += opacity * 0.1;
+        }, 10);
+    }
+    
+    validate_form("#form_1");
+}
+
+export default validate;
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
+import validate from "./blocks/validate.js";
+
 window.addEventListener('DOMContentLoaded', () => {
+
     new WOW().init();
+    validate()
+    
 })
