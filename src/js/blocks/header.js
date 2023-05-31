1e2de8af53b2923f934f7e4711753b7dbc6c7832
Traniks <andert2017@gmail.com>
2023-05-31 19:26:23 +0300

    adaptive + hamburger + head_fixed

---
+++ src/js/blocks/header.js
@@ -0,0 +1,33 @@
+function header() {
+
+    // header list
+    const menu = document.querySelector('.header__list'),
+    menuItem = document.querySelectorAll('.header__list-item'),
+    hamburger = document.querySelector('.header__hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('header__hamburger_active');
+        menu.classList.toggle('header__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__list_active');
+        })
+    })
+
+    //header fixed
+    const header = document.querySelector(".header");
+
+    window.addEventListener("scroll", () => {
+        if (scrollY > 600) {
+            header.classList.add("header_active");
+        } else {
+            header.classList.remove("header_fixed");
+        }
+    })
+
+}
+
+export default header;
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import validate from "./blocks/validate.js";
 import modal from "./blocks/modal.js";
+import header from "./blocks/header.js";
 
 window.addEventListener('DOMContentLoaded', () => {
 
     new WOW().init();
     validate();
     modal();
+    header();
 
 })
